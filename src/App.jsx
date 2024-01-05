4d5c4daabbae3671edced09a39ef4a93e930ace2
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2024-01-05 23:41:27 +0700

    Style: Header & Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import "./App.css";
+import MyBalance from "./assets/components/MyBalance";
+import Summary from "./assets/components/Summary";
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <main className="min-w-full h-full flex flex-col justify-center items-center">
+      <MyBalance />
+      <Summary />
+    </main>
+  );
 }
-export default App
+export default App;
---
+++ src/assets/components/MyBalance.jsx
@@ -0,0 +1,19 @@
+import "../sass/components/MyBalance.scss";
+
+function MyBalance() {
+  return (
+    <section
+      id="myBalance"
+      className="container w-[90%] flex flex-row justify-between items-center px-6 py-5 text-white"
+    >
+      <div>
+        <h4 className="text-[17px]">My balance</h4>
+        <p className="text-[28px] font-[500] mt-1">$921.48</p>
+      </div>
+      <div>
+        <img src="../../../images/logo.svg" alt="App-logo" />
+      </div>
+    </section>
+  );
+}
+export default MyBalance;
