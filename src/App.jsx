3f0776466df1ef374a3c7520c83dc8e4e100015f
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2024-01-23 11:16:15 +0700

    Feat: createContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import "./App.css";
+import DataContext from "./data/contextData"
+import jsonData from "./data/data.json"
 import MyBalance from "./assets/components/MyBalance";
 import Graph from "./assets/components/GraphSection";
 import Summary from "./assets/components/Summary";
 
 function App() {
+
   return (
-    <main className="min-w-full h-full flex flex-col justify-center items-center">
-      <MyBalance />
-      <Graph />
-      <Summary />
-    </main>
+    <DataContext.Provider value={ jsonData }>
+      <main className="min-w-full h-full flex flex-col justify-center items-center">
+        <MyBalance />
+        <Graph />
+        <Summary />
+      </main>
+    </DataContext.Provider>
   );
 }
 export default App;
