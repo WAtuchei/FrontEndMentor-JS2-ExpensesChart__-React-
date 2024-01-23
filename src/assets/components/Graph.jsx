c4c05043b09036fab941796f69c4dea68b0247e7
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2024-01-23 15:25:40 +0700

    Feat: GraphDay components

--- src/assets/components/Graph.jsx
+++ src/assets/components/Graph.jsx
@@ -1,33 +1,44 @@
-import "../sass/components/Graph.scss"
+import { useContext, useState } from "react"
+import "../../sass/components/GraphSection.scss"
+import DataContext from "../../data/contextData"
+import PropTypes from "prop-types"
 
 function Graph() {
-  return (
-    <ul
-      className="container w-full flex flex-row flex-nowrap gap-x-3 mt-4"
-    >
-      <li className="border border-red-500 flex-1">
-         <p>mon</p>
-      </li>
-      <li className="border border-red-500 flex-1">
-         <p>mon</p>
-      </li>
-      <li className="border border-red-500 flex-1">
-         <p>mon</p>
-      </li>
-      <li className="border border-red-500 flex-1">
-         <p>mon</p>
-      </li>
-      <li className="border border-red-500 flex-1">
-         <p>mon</p>
-      </li>
-      <li className="border border-red-500 flex-1">
-         <p>mon</p>
-      </li>
-      <li className="border border-red-500 flex-1">
-         <p>mon</p>
-      </li>
+  const graphData = useContext(DataContext)
+  const [graphIsTop, setGraphIsTop] = useState('')
 
-    </ul>
+  function GraphDay({data}) {
+    
+    return (
+      <li className="flex flex-col flex-1 items-center">
+           <div className={`Graph-day${graphIsTop} w-full h-full`}></div>
+           <p className="thin-Text mt-3"
+           >
+              {`${data.day}`}
+           </p>
+      </li>
+    )
+  }
+
+  GraphDay.propTypes = {
+    data: PropTypes.shape({
+      day: PropTypes.string.isRequired,
+      amount: PropTypes.number.isRequired
+    })
+  }
+
+  return (
+    <>
+      <ul
+        className="container w-full flex flex-row flex-nowrap gap-x-4 mt-10"
+      >
+        {graphData.map((data, index) =>{
+          return <GraphDay key={index} data={data} />
+        })}
+      </ul>
+
+      <div className="break-line w-full mt-7"></div>
+    </>
   )
 }
 export default Graph
